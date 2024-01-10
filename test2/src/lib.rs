3def8eadd0c464d0245422acde741582bb2f1aa4
David Wallace Croft <david@CroftSoft.com>
2024-01-10 11:50:12 -0600

    cleanup

--- test2/src/lib.rs
+++ test2/src/lib.rs
@@ -5,34 +5,37 @@ use spin_sdk::{
   key_value::Store,
 };
 
+const KEY: &str = "key";
+const KEY_NOT_FOUND: &str = "key not found";
+const PATH: &str = "/test2/:key";
+
 #[http_component]
-fn handle_request(req: Request) -> Response {
+fn handle_request(request: Request) -> Response {
   let mut router = Router::new();
-  router.delete("/test2/:key", handle_delete);
-  router.get("/test2/:key", handle_get);
-  router.head("/test2/:key", handle_head);
-  router.post("/test2/:key", handle_post);
-  router.handle(req)
+  router.delete(PATH, handle_delete);
+  router.get(PATH, handle_get);
+  router.head(PATH, handle_head);
+  router.post(PATH, handle_post);
+  router.handle(request)
 }
 
 fn handle_delete(
-  _req: Request,
+  _request: Request,
   params: Params,
 ) -> anyhow::Result<impl IntoResponse> {
-  let key = params.get("key").expect("key not found");
-  println!("Delete key {key}");
-  let store = Store::open_default()?;
+  let key: &str = params.get(KEY).expect(KEY_NOT_FOUND);
+  let store: Store = Store::open_default()?;
   store.delete(key)?;
-  Ok(Response::new(StatusCode::OK, None::<Vec<u8>>))
+  let response = Response::new(StatusCode::OK, None::<Vec<u8>>);
+  Ok(response)
 }
 
 fn handle_get(
-  _req: Request,
+  _request: Request,
   params: Params,
 ) -> anyhow::Result<impl IntoResponse> {
-  let key = params.get("key").expect("key not found");
-  println!("Get key {key}");
-  let store = Store::open_default()?;
+  let key: &str = params.get(KEY).expect(KEY_NOT_FOUND);
+  let store: Store = Store::open_default()?;
   let value: Option<Vec<u8>> = store.get(key)?;
   match value {
     Some(value) => Ok(Response::new(StatusCode::OK, value)),
@@ -41,28 +44,29 @@ fn handle_get(
 }
 
 fn handle_head(
-  _req: Request,
+  _request: Request,
   params: Params,
 ) -> anyhow::Result<impl IntoResponse> {
-  let key = params.get("key").expect("key not found");
-  println!("Head key {key}");
-  let store = Store::open_default()?;
+  let key: &str = params.get(KEY).expect(KEY_NOT_FOUND);
+  let store: Store = Store::open_default()?;
   let value: Option<Vec<u8>> = store.get(key)?;
-  let code = if value.is_some() {
+  let code: StatusCode = if value.is_some() {
     StatusCode::OK
   } else {
     StatusCode::NOT_FOUND
   };
-  Ok(Response::new(code, None::<Vec<u8>>))
+  let response = Response::new(code, None::<Vec<u8>>);
+  Ok(response)
 }
 
 fn handle_post(
-  req: Request,
+  request: Request,
   params: Params,
 ) -> anyhow::Result<impl IntoResponse> {
-  let key = params.get("key").expect("key not found");
-  println!("Post key {key}");
-  let store = Store::open_default()?;
-  store.set(key, req.body())?;
-  Ok(Response::new(StatusCode::OK, None::<Vec<u8>>))
+  let key: &str = params.get(KEY).expect(KEY_NOT_FOUND);
+  let store: Store = Store::open_default()?;
+  let request_body: &[u8] = request.body();
+  store.set(key, request_body)?;
+  let response = Response::new(StatusCode::OK, None::<Vec<u8>>);
+  Ok(response)
 }
