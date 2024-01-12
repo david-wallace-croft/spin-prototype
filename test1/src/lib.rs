7117bafcd2bc9d7a1d2bb8f744c3de8742aaffc2
David Wallace Croft <david@CroftSoft.com>
2024-01-12 11:27:21 -0600

    string extract

--- test1/src/lib.rs
+++ test1/src/lib.rs
@@ -7,8 +7,14 @@ fn handle_request(request: Request) -> Result<impl IntoResponse> {
   let header_value_option: Option<&HeaderValue> =
     request.header("spin-full-url");
   let body: String = match header_value_option {
-    Some(header_value) => format!("spin-full-url: {:?}", header_value),
-    None => "spin-full-url not provided".to_owned(),
+    Some(header_value) => {
+      let str_option: Option<&str> = header_value.as_str();
+      match str_option {
+        Some(str_value) => format!("Header spin-full-url: {str_value}"),
+        None => "Header spin-full-url is not a string".to_owned(),
+      }
+    },
+    None => "Header spin-full-url is not set".to_owned(),
   };
   Ok(
     http::Response::builder()
